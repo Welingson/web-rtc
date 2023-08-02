11b7a76e2dd66a1572b6389afa339bb966769bf3
Welingson <welingson12@gmail.com>
2023-08-02 16:32:26 -0300

    update env file

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_SOCKET_SERVER, {
+export const socket = io(import.meta.env.VITE_API_URL, {
     autoConnect: true
 });
