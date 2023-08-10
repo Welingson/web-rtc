fc4155219e7f8158626fb7d14de9114e478ef1de
Welingson <welingson12@gmail.com>
2023-08-10 11:58:39 -0300

    add token jwt in socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,23 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_API_URL, {
-    autoConnect: true
-});
+let socket = null;
+
+const initClientIO = () => {
+    socket = io(import.meta.env.VITE_API_URL, {
+        autoConnect: true,
+        auth: {
+            token: localStorage.getItem('user')
+                ? JSON.parse(localStorage.getItem('user')).token : false
+        }
+    });
+
+    return socket;
+}
+
+export const getClientIO = () => {
+
+    if (!socket) {
+        return initClientIO()
+    }
+    return socket;
+}
