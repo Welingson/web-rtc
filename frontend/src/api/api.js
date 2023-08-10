141abd06f949c1a156cfe57775b9aadf5601172d
Welingson <welingson12@gmail.com>
2023-08-10 11:58:01 -0300

    add bearer auth

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-
+const configApi = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
 })
+
+configApi.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem('user')
+            ? JSON.parse(localStorage.getItem('user')).token : false;
+
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => { Promise.reject(error) }
+)
+
+export const api = configApi;
